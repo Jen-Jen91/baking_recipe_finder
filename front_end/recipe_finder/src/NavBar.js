f309d9972d7be364a8304130f9d10e49038b83dd
Jennifer Proctor <jen_p_13@hotmail.co.uk>
2018-12-15 15:00:48 +0000

    Added NavBar links

--- front_end/recipe_finder/src/NavBar.js
+++ front_end/recipe_finder/src/NavBar.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const NavBar = (props) => {
   return (
     <header>
       <ul>
-        <li>Home</li>
-        <li>Recipes</li>
-        <li>Ingredients</li>
+        <li className="navLink"><Link to="/">Home</Link></li>
+        <li className="navLink"><Link to="/recipes">Recipes</Link></li>
+        <li className="navLink"><Link to="/ingredients">Ingredients</Link></li>
       </ul>
     </header>
   );
